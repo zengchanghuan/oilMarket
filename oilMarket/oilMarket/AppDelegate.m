5e2af823e47bc016a3d86d2e632e3d83d9078933
ZengChanghuan <zengchanghuan@gmail.com>
2020-05-04 16:18:59 +0800

    add file and fix settings

--- oilMarket/oilMarket/AppDelegate.m
+++ oilMarket/oilMarket/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#define kScreenBounds [UIScreen mainScreen].bounds
 
 @interface AppDelegate ()
 
@@ -17,7 +18,11 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    return YES;
+    self.window = [[UIWindow alloc] initWithFrame:kScreenBounds];
+    
+    self.window.rootViewController = [[UIViewController alloc] init];
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];    return YES;
 }
 
 
