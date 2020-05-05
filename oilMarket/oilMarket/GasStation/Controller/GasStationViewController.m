23604128755d50d12446e3bc0f6cb521623a5b4e
ZengChanghuan <zengchanghuan@gmail.com>
2020-05-05 14:15:41 +0800

    add SegmentedControl

--- oilMarket/oilMarket/GasStation/Controller/GasStationViewController.m
+++ oilMarket/oilMarket/GasStation/Controller/GasStationViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "GasStationViewController.h"
-
+#import "NYSegmentedControl.h"
 @interface GasStationViewController ()
 
 @end
@@ -16,18 +16,35 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor purpleColor];
-    
-   
+    NYSegmentedControl *seq = [self blueSegmentedControl];
+    seq.frame = CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width-80, 44);
+    self.navigationItem.titleView = seq;
+    [seq addTarget:self action:@selector(selectItem:) forControlEvents:UIControlEventValueChanged];
+}
+
+- (NYSegmentedControl *)blueSegmentedControl {
+    NYSegmentedControl *blueSegmentedControl = [[NYSegmentedControl alloc] initWithItems:@[@"默认通道", @"柴油通道"]];
+    blueSegmentedControl.titleTextColor = [UIColor colorWithRed:0.38f green:0.68f blue:0.93f alpha:1.0f];
+    blueSegmentedControl.selectedTitleTextColor = [UIColor whiteColor];
+    blueSegmentedControl.segmentIndicatorBackgroundColor = [UIColor colorWithRed:0.38f green:0.68f blue:0.93f alpha:1.0f];
+    blueSegmentedControl.backgroundColor = [UIColor colorWithRed:0.31f green:0.53f blue:0.72f alpha:1.0f];
+    blueSegmentedControl.borderWidth = 0.0f;
+    blueSegmentedControl.segmentIndicatorBorderWidth = 0.0f;
+    blueSegmentedControl.segmentIndicatorInset = 2.0f;
+    blueSegmentedControl.segmentIndicatorBorderColor = self.view.backgroundColor;
+    blueSegmentedControl.cornerRadius = blueSegmentedControl.intrinsicContentSize.height / 2.0f;
+    blueSegmentedControl.usesSpringAnimations = YES;
+    return blueSegmentedControl;
 }
 
 - (void)selectItem:(UISegmentedControl *)sender {
     if (sender.selectedSegmentIndex == 0) {
-        NSLog(@"正在销售");
+        NSLog(@"默认通道");
     } else {
-        NSLog(@"已下架");
+        NSLog(@"柴油通道");
     }
 }
+
 
 
 - (void)didReceiveMemoryWarning {
